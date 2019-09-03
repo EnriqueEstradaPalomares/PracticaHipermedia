2a3c0af045acf58fb1ab2954836e419b7826818a
20132792 JOSE ENRIQUE ESTRADA PALOMARES <20132792@TELEMATICA.UCOL.MX>
2019-09-03 08:37:58 -0500

    avance a Google Oath

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 var express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys')
 var app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, accessToken => {
+    console.log(accessToken);
+})
+);
 
 
-const PORT = process.env.PORT || 3000;
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['porfile', 'email']
+    })
+);
+
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
 
@@ -14,4 +30,5 @@ app.listen(PORT);
 //npm install --save express
 //npm install --nodemon
 //npm install --save passport-google-oauth20
-
+//ID client: 237688805656-hcqqt40lquu60okurnc547l525ve863k.apps.googleusercontent.com
+//ID Secret: Lf9wat2EtKsWcg-aDju77g0i
